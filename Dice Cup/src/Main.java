05dee520e60f0add0a2ec51afeec68036c8334ad
ajames191 <40630985+ajames191@users.noreply.github.com>
2019-03-08 21:54:54 +0000

    Dice Cup

---
+++ Dice Cup/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 0;
+        int m = 0;
+
+        System.out.println(mostLikelySum(n, m));
+    }
+
+    public static int mostLikelySum(int n, int m) {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            //
+        }
+
+        return sum;
+    }
+}
+
+/*
+d4 or D4 indicates a die with 4 sides
+If several dice of the same type are rolled, this is indicated by a leading number:
+2D6 means that two dies of sixes should be rolled by the player
+
+Input consists of two ints: N, M, specifying number of faces of the two die
+
+N <= 4
+M <= 20
+
+*/
+
